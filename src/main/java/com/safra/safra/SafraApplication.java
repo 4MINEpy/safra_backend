55d38fba3d84dfe2188ea5eccde119056b5fe86d
4MINEpy <aminmedfai30@gmail.com>
2025-11-05 20:37:39 +0100

    first commit

---
+++ src/main/java/com/safra/safra/SafraApplication.java
@@ -0,0 +1,13 @@
+package com.safra.safra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safra/safra/SafraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safra.safra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
