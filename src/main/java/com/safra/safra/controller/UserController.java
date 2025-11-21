54a694200a822274a11b62d4a3592ce7bae11fef
4MINEpy <aminmedfai30@gmail.com>
2025-11-21 11:39:29 +0100

    added users API

--- src/main/java/com/safra/safra/controller/UserController.java
+++ src/main/java/com/safra/safra/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.safra.safra.controller;
 
+import com.safra.safra.entity.User;
+import com.safra.safra.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or @userSecurity.isSelf(#id)")
+    public ResponseEntity<?> getUserById(@PathVariable Long id) {
+
+        User user = userRepository.findById(id).orElse(null);
+
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("User not found");
+        }
+        return ResponseEntity.ok(user);
+    }
 }
